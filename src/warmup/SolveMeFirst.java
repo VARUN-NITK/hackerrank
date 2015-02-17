10ed7fd1d4d398891b25b175ae2e5619d4eadd16
varun <varunnitk.jain@gmail.com>
2015-02-17 20:27:08 +0530

    warmup - SolveMeFirst commit

---
+++ src/warmup/SolveMeFirst.java
@@ -0,0 +1,26 @@
+package warmup;
+
+import java.util.Scanner;
+
+/**
+ * The task is to scan two numbers from STDIN, and print the sum A+B on STDOUT
+ * <p/>
+ * Created by Varun on 17-02-2015.
+ */
+public class SolveMeFirst {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
